7ddeca9d3ad62911cbe690fccd2698c4d93fc496
Shaheen Alam <shaheen@4axiz.com>
2018-09-28 11:06:02 +0600

    Add all full project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'iformbuilder';
+	title = 'Iformbuilder APP';
+	access_token = '';
+
+	constructor(private http: HttpClient) {
+		console.log('Hello RestServiceProvider Provider');
+	}
+
+	getAccessToken = function(){
+		this.http.get('http://fouraxiz.com/iformbuilder/index.php').subscribe(result => {
+			this.access_token = result.token;
+		});
+	}
+
+	saveData = function(ev){
+		var formData = new FormData();
+		console.log(formData);
+	//	this.http.get('http://fouraxiz.com/iformbuilder/index.php').subscribe(result => {
+		//	this.access_token = result.token;
+	//	});
+	}
 }
+
+
